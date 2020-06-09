135ceed7d704703f693d86a04bd9fa3714f73261
Mykyta Kamak <kamaknikita@gmail.com>
2020-06-09 10:59:38 +0300

    add root user

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,3 +4,8 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/ > /dev/null 2>&1
 cp -r ~/Downloads/dotfiles/vim/* ~/.vim/
 ln -s ~/Downloads/dotfiles/vim/.vimrc ~/.vimrc
+
+# setup root user
+mkdir /root/.vim/ > /dev/null 2>&1
+cp -r ~/Downloads/dotfiles/vim/* /root/.vim/
+sudo ln -s ~/Downloads/dotfiles/vim/.vimrc /root/.vimrc
