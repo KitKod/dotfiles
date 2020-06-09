19657dcd6df0e77ee00ee4bab0a18ff6fc848bfd
Mykyta Kamak <Mykyta.Kamak@portaone.com>
2020-06-09 10:42:15 +0300

    Edite

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y install fonts-powerline
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-git clone https://github.com/KitKod/dotfiles.git ~/Downloads/dotfiles/
+mkdir ~/.vim/ > /dev/null 2>&1
 cp -r ~/Downloads/dotfiles/vim/* ~/.vim/
-
 ln -s ~/Downloads/dotfiles/vim/.vimrc ~/.vimrc
